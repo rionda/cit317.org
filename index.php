44544f77ea61f8f2d3bad4a2f957b0bb5c9e6979
Matteo Riondato <riondato@acm.org>
2016-09-11 18:10:14 -0400

    Add a first page.

---
+++ index.php
@@ -0,0 +1,49 @@
+ <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+ <!-- vim: set ts=2 sw=2 tw=79: -->
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<link href='https://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css' />
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+	<meta name="author" content="Matteo Riondato" />
+	<meta name="description" content="CIT 317 @ Brown University CS Dept. -- The
+Andy Pavlo Memorial Office, the ultimate graduate student office." />
+	<meta name="revised" content="<?php echo date("D M j G:i:s T Y", getlastmod()) ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>CIT 317 &mdash; The ultimate graduate student office</title>
+	<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<h1>CIT 317 &mdash; The Andy Pavlo Memorial Office</h1>
+<h2>The ultimate graduate student office</h2>
+<p>This page honors CIT 317, an office on the 3rd floor of the Center for
+Information Technology at Brown University, part of the Computer Science
+Department.<p>
+<p>CIT 317 has been, over the course of its existences, a graduate student
+office (mostly, if not only, reserved to Ph.D. students), a postdoc office, and
+a visiting faculty office.<p>
+<p>Since May 2014, CIT 317 is also the <a href="http://cs.cmu.edu/~pavlo/">Andy
+Pavlo</a> Memorial Office, to honor one of its most famous occupants.</p>
+<p>CIT 317 has two desks, but three scholars can comfortably sit in it, and up
+to four occupants can work productively.<p>
+
+<h3>Occupants Honor Roll</h3>
+<ol>
+<li><a href="http://matteo.rionda.to">Matteo Riondato</a>, Postdoc and Visiting
+Assistant Professor, from February 2015. </li>
+<li><a href="http://www.epasto.org">Alessandro Epasto</a>, Postdoc, from January
+2015 to March 2016</li>
+<li><a href="http://cs.brown.edu/~mace">Jonathan Mace</a>, Ph.D. student.</li>
+<li><a href="http://cs.cmu.edu/~pavlo">Andy Pavlo</a>, Ph.D. Student, Gentleman, Lover, and Scholar.</li>
+</ol>
+<p>For any addition or correction to the honor roll, please contact Matteo
+Riondato</p>
+
+<p>&copy; <?php echo date("Y")?> <a xmlns:cc="http://creativecommons.org/ns#"
+href="http://matteo.rionda.to" property="cc:attributionName"
+rel="cc:attributionURL">Matteo Riondato</a> &mdash; Licensed under a <a
+rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative
+Commons Attribution-ShareAlike 4.0 International License</a> &mdash; Last
+modified: <?php echo date ("F d Y H:i:s T", getlastmod()); ?></p>
+</body>
+</html>
